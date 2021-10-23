ad52d8e0ae72ad9e3ddbdfa37b5920f889ce01d9
Ken329 <ken_037729@hotmail.com>
2021-10-23 20:26:55 +0800

    done generating table function along with printing function

--- src/pages/Qrpage.js
+++ src/pages/Qrpage.js
@@ -9,7 +9,8 @@ import 'react-toastify/dist/ReactToastify.css';
 import { Dialog, Transition } from '@headlessui/react'
 import Qrcode from 'qrcode.react';
 import { XIcon } from '@heroicons/react/outline'
-import { PlusCircle } from 'heroicons-react';
+import { PlusCircle, Printer } from 'heroicons-react';
+import printJS from 'print-js';
 
 const cookies = new Cookies();
 
@@ -101,6 +102,7 @@ function TracksPage() {
                         autoClose: 3000
                     });
                     setSideBar(false);
+                    setsideBarTitle("")
                     getGeneratedData(id);
                 }
             })
@@ -110,6 +112,79 @@ function TracksPage() {
                 autoClose: 3000
             });
         }
+    }
+
+    // update data function
+    function updateTableFunction(generateId){
+        const id = cookies.get("user_id")
+        var input = document.getElementsByClassName("update-table");
+        var data = [];
+        var check = true;
+        for(var myInput of input){
+            if(myInput.value === ""){
+                check = false;
+                break;
+            }
+            data.push(myInput.value);
+        }
+        var index = generateData.findIndex(x => 
+            x.table_no === data[0] && 
+            x.description === data[1]
+        );
+        if(index !== -1){
+            toast.info("No changes has been made", {
+                position: toast.POSITION.TOP_RIGHT,
+                autoClose: 3000
+            });
+            setSideBar(false);
+        }
+        if(check){
+            const myData = {
+                table_no: data[0],
+                description: data[1]
+            }
+            Axios.put("https://eatsy-0329.herokuapp.com/updateGenerateData", {
+                id: id,
+                tableId: generateId,
+                tableDetail: myData
+            })
+            .then((res) => {
+                if(res.data.success){
+                    toast.success(res.data.data, {
+                        position: toast.POSITION.TOP_RIGHT,
+                        autoClose: 3000
+                    });
+                    setSideBar(false);
+                    setsideBarTitle("")
+                    setSideBarIndex(0);
+                    getGeneratedData(id);
+                }
+            })
+        }else{
+            toast.error("Do not leave any field empty", {
+                position: toast.POSITION.TOP_RIGHT,
+                autoClose: 3000
+            });
+        }
+    }
+
+    // delete generate function
+    function deleteGenerateFunction(generateId){
+        const id = cookies.get("user_id");
+        Axios.post("https://eatsy-0329.herokuapp.com/deleteGenerateData", {
+            id: id,
+            tableId: generateId
+        })
+        .then((res) => {
+            if(res.data.success){
+                toast.success(res.data.data, {
+                    position: toast.POSITION.TOP_RIGHT,
+                    autoClose: 3000
+                });
+                setSideBarIndex(0);
+                getGeneratedData(id);
+            }
+        })
     }
 
     return (
@@ -126,7 +201,7 @@ function TracksPage() {
                         last_name={"Ken"} 
                         first_name={"Liau"} 
                         gender={userDetail.user_gender}
-                        section={3}
+                        section={2}
                     />
                     <header className="bg-white shadow">
                         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
@@ -192,7 +267,7 @@ function TracksPage() {
                                             sideBarTitle === "Create New Table" ? 
                                             <div className="px-5 flex-col">
                                                 <p className="px-3 mt-2">Table No: </p>
-                                                <input className="w-full rounded-full bg-gray-200 px-3 py-1 my-2 outline-none new-table" 
+                                                <input className="w-full rounded-xl bg-gray-200 px-3 py-1 my-2 outline-none new-table" 
                                                 placeholder="New table no"/>
                                                 <p className="px-3 mt-2">Description: </p>
                                                 <textarea className="w-full rounded-xl bg-gray-200 px-3 py-2 my-2 outline-none new-table" 
@@ -207,7 +282,7 @@ function TracksPage() {
                                             </div>
                                             : <div className="px-5 flex-col">
                                                 <p className="px-3 mt-2">Table No: </p>
-                                                <input className="w-full rounded-full bg-gray-200 px-3 py-1 my-2 outline-none update-table" 
+                                                <input className="w-full rounded-xl bg-gray-200 px-3 py-1 my-2 outline-none update-table" 
                                                 placeholder="Update table no"
                                                 defaultValue={
                                                     generateData.length !== 0 
@@ -224,6 +299,7 @@ function TracksPage() {
                                                 }/>
                                                 <div className="flex justify-end mt-6">
                                                     <button 
+                                                    onClick={e => updateTableFunction(generateId[sideBarIndex])}
                                                     className="bg-gray-200 text-gray-700 px-3 py-2 rounded-3xl hover:bg-gray-300 hover:text-gray-900">
                                                         Update
                                                     </button>
@@ -244,61 +320,108 @@ function TracksPage() {
                                 setSideBar(true)
                                 setsideBarTitle("Create New Table")
                             }}
-                            className="bg-white px-4 py-3 rounded-full flex">
+                            className="bg-white px-3 py-2 rounded-full flex mx-3">
                                 <PlusCircle className="w-6 h-6 mr-2"/>
                                 Create
                             </button>
+                            <button
+                            onClick={e => {
+                                printJS({
+                                    printable: 'all_qr',
+                                    type: 'html',
+                                    targetStyles: ['*']
+                                })
+                            }}
+                            className="bg-white px-3 py-2 rounded-full flex mx-3">
+                                <Printer className="w-6 h-6 mr-2"/>
+                                Print
+                            </button>
                         </div>
-                        <h3 className="w-4/5 py-4 px-6 mx-auto font-bold">Restaurant</h3>
-                        <section className="w-4/5 mx-auto grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
-                            <div className="bg-white h-auto rounded-2xl m-3">
-                                <div className="w-full h-auto text-sm pt-2 font-semibold rounded-t-2xl flex justify-center items-center">
-                                    <img className="w-10 h-10 rounded-full m-1" src={userDetail.user_image}/>
-                                    <h4 className="m-1 font-serif">{userDetail.user_restaurant}</h4>
-                                </div>
-                                <Qrcode 
-                                    className="w-4/5 mx-auto mt-6"
-                                    value={"https://ken329.github.io/Web_MajorProject_CustomerView/menu.html?restaurantID="+userId}
-                                />
-                                <div className="w-full flex justify-center items-center py-4 font-semibold">Scan here to get menu</div>
-                            </div>
-                        </section>
-                        <h3 className="w-4/5 py-4 px-6 mx-auto font-bold">Table</h3>
-                        <section className="w-4/5 mx-auto grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
-                            {
-                                generateData.map((data, index) => {
-                                    return <div key={index} className="bg-white h-auto rounded-2xl m-3 pb-5 relative overflow-hidden">
+                        <div id="all_qr" className="flex-col w-4/5 mx-auto">
+                            <h3 className="w-full py-4 px-6 mx-auto font-bold">Restaurant</h3>
+                            <section className="w-full mx-auto grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+                                <div 
+                                id="qr"
+                                className="bg-white h-auto rounded-2xl m-3 relative">
                                     <div className="w-full h-auto text-sm pt-2 font-semibold rounded-t-2xl flex justify-center items-center">
                                         <img className="w-10 h-10 rounded-full m-1" src={userDetail.user_image}/>
                                         <h4 className="m-1 font-serif">{userDetail.user_restaurant}</h4>
                                     </div>
+                                    <div 
+                                        onClick={e => {
+                                            printJS({
+                                                printable: 'qr',
+                                                type: 'html',
+                                                targetStyles: ['*']
+                                            })
+                                        }}
+                                        className="p-1 rounded-full bg-gray-100 absolute top-1 right-1 cursor-pointer hover:bg-gray-200 print-logo">
+                                            <Printer className="w-5 h-5"/>
+                                        </div>
                                     <Qrcode 
                                         className="w-4/5 mx-auto mt-6"
-                                        value={"https://ken329.github.io/Web_MajorProject_CustomerView/menu.html?restaurantID="+userId+"&table_no="+data.table_no}
+                                        value={"https://ken329.github.io/Web_MajorProject_CustomerView/menu.html?restaurantID="+userId}
                                     />
-                                    <div className="w-full flex justify-center items-center px-2 py-4 font-semibold">{data.description}</div>
-                                    <div className="absolute rounded-lg left-0 right-0 bottom-0 bg-gray-200 transform translate-y-2/3 
-                                    ease-in-out duration-700 hover:translate-y-0">
-                                        <p className="w-10/12 mx-auto mb-2 text-center text-gray-700">Table No : {data.table_no}</p>
-                                        <div className="w-full flex">
-                                            <button 
-                                            onClick={e => {
-                                                setSideBar(true)
-                                                setsideBarTitle("Updating Table")
-                                                setSideBarIndex(index)
-                                            }}
-                                            className="w-1/2 text-center rounded-2xl py-2 text-gray-700 hover:bg-gray-300 hover:text-gray-900">
-                                                Edit
-                                            </button>
-                                            <button className="w-1/2 text-center rounded-2xl py-2 text-gray-700 hover:bg-gray-300 hover:text-gray-900">
-                                                Delete
-                                            </button>
-                                        </div>
-                                    </div>
+                                    <div className="w-full flex justify-center items-center py-4 font-semibold">Scan here to get menu</div>
                                 </div>
-                                })
+                            </section>
+                            <h3 className="w-full py-4 px-6 mx-auto font-bold">Table</h3>
+                            {
+                                generateData.length === 0
+                                ? <h3 className="w-full text-center text-gray-600 font-bold">No generated table found</h3>
+                                : <section className="w-full mx-auto grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+                                    {
+                                        generateData.map((data, index) => {
+                                            return <div 
+                                            key={index} 
+                                            id={"qr_"+generateId[index]} 
+                                            className="bg-white h-auto rounded-2xl m-3 pb-5 relative overflow-hidden">
+                                            <div className="w-full h-auto text-sm pt-2 font-semibold rounded-t-2xl flex justify-center items-center">
+                                                <img className="w-10 h-10 rounded-full m-1" src={userDetail.user_image}/>
+                                                <h4 className="m-1 font-serif">{userDetail.user_restaurant}</h4>
+                                            </div>
+                                            <div 
+                                            onClick={e => {
+                                                printJS({
+                                                    printable: 'qr_'+generateId[index],
+                                                    type: 'html',
+                                                    targetStyles: ['*']
+                                                })
+                                            }}
+                                            className="p-1 rounded-full bg-gray-100 absolute top-1 right-1 cursor-pointer hover:bg-gray-200 print-logo">
+                                                <Printer className="w-5 h-5"/>
+                                            </div>
+                                            <Qrcode 
+                                                className="w-4/5 mx-auto mt-6"
+                                                value={"https://ken329.github.io/Web_MajorProject_CustomerView/menu.html?restaurantID="+userId+"&table_no="+data.table_no}
+                                            />
+                                            <div className="w-full flex justify-center items-center px-2 py-4 font-semibold">{data.description}</div>
+                                            <div className="absolute rounded-lg left-0 right-0 bottom-0 bg-gray-200 transform translate-y-2/3 
+                                            ease-in-out duration-700 hover:translate-y-0">
+                                                <p className="w-10/12 mx-auto mb-2 text-center text-gray-700">Table No : {data.table_no}</p>
+                                                <div className="w-full flex">
+                                                    <button 
+                                                    onClick={e => {
+                                                        setSideBar(true)
+                                                        setsideBarTitle("Updating Table")
+                                                        setSideBarIndex(index)
+                                                    }}
+                                                    className="w-1/2 text-center rounded-2xl py-2 text-gray-700 hover:bg-gray-300 hover:text-gray-900">
+                                                        Edit
+                                                    </button>
+                                                    <button 
+                                                    onClick={e => deleteGenerateFunction(generateId[index])}
+                                                    className="w-1/2 text-center rounded-2xl py-2 text-gray-700 hover:bg-gray-300 hover:text-gray-900">
+                                                        Delete
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        })
+                                    }
+                                </section>
                             }
-                        </section>
+                        </div>
                     </main>
                     </>
                 )
