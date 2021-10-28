8092741b150ff76e9532ccfb0121af0ad91c7e50
Ken329 <ken_037729@hotmail.com>
2021-10-28 21:45:25 +0800

    bugs fixed

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -113,7 +113,8 @@ function Login() {
     return (false);
   }
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen w-full">
+    <div className="flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <ToastContainer />
       <div className="max-w-md w-full space-y-8">
         <div>
@@ -263,7 +264,8 @@ function Login() {
           </form>
         }
       </div>
-      <Footer />
+    </div>
+    <Footer />
     </div>
   )
 }
