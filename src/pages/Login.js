1b33389603b6c68409af98239c6c9a96b9f0108f
Ken329 <ken_037729@hotmail.com>
2021-10-29 12:54:22 +0800

    ui bugs on login page fixed

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -113,8 +113,8 @@ function Login() {
     return (false);
   }
   return (
-    <div className="min-h-screen w-full">
-    <div className="flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen w-full bg-gray-100 ">
+    <div className="flex min-h-screen items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <ToastContainer />
       <div className="max-w-md w-full space-y-8">
         <div>
